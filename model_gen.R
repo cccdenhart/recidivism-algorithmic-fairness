50bc938b47e3a494c447f34e07c86024c789f3bf
Aaron Moskowitz <amoskowitz14@gmail.com>
2020-12-18 11:44:40 -0500

    changed file names from c_charge_degree to priors_count

--- model_gen.R
+++ model_gen.R
@@ -1,48 +1,25 @@
 # generate all models with bnlearn and save probabilities
 
-setwd("~/neu/cs7290/final-project")
+#setwd("~/neu/cs7290/final-project")
 
 library(dplyr)
 library(bnlearn)
 library(Rgraphviz)
 
 # read in simplified compas data
-#df <- read.csv('data/compas-scores-two-years_short.csv', stringsAsFactors=TRUE)
-df <- read.csv('data/compas-scores-two-years_short-priors_count.csv', stringsAsFactors=TRUE)
+df <- read.csv('data/compas-scores-two-years-short.csv', stringsAsFactors=TRUE)
 
 df <- df %>%
   mutate(two_year_recid = factor(two_year_recid)) %>%
-  select(0:ncol(df))
+  mutate(priors_count = factor(priors_count))
 
-# ==========================================================================================
-# print DAGs
 nodes = c("race", "sex", "age_cat", "priors_count", "two_year_recid")
 e = empty.graph(nodes)
 
+# ==========================================================================================
 # Model 1
+
 modelstring(e) = "[race][sex][age_cat][priors_count|race:sex:age_cat][two_year_recid|priors_count]"
-dag = model2network(modelstring(e), ordering = nodes)
-graphviz.plot(dag)
-
-# Model 2
-modelstring(e) = "[race][sex][age_cat][priors_count|race:sex:age_cat][two_year_recid|priors_count:race]"
-dag = model2network(modelstring(e), ordering = nodes)
-graphviz.plot(dag)
-
-# Model 3
-modelstring(e) = "[race][sex][age_cat][priors_count|sex:age_cat][two_year_recid|priors_count]"
-dag = model2network(modelstring(e), ordering = nodes)
-graphviz.plot(dag)
-
-# ==========================================================================================
-# Calculation of model parameters
-nodes = c("race", "sex", "age_cat", "c_charge_degree", "two_year_recid")
-
-# ==========================================================================================
-# model 1
-
-e = empty.graph(nodes)
-modelstring(e) = "[race][sex][age_cat][c_charge_degree|race:sex:age_cat][two_year_recid|c_charge_degree]"
 dag = model2network(modelstring(e), ordering = nodes)
 graphviz.plot(dag)
 
@@ -51,37 +28,33 @@ model1 = bn.fit(x = dag, data = df)
 write.csv(model1$race$prob,"data/model1_race.csv", row.names = TRUE)
 write.csv(model1$sex$prob,"data/model1_sex.csv", row.names = TRUE)
 write.csv(model1$age_cat$prob,"data/model1_age_cat.csv", row.names = TRUE)
-write.csv(model1$c_charge_degree$prob,"data/model1_c_charge_degree.csv", row.names = TRUE)
+write.csv(model1$priors_count$prob,"data/model1_priors_count.csv", row.names = TRUE)
 write.csv(model1$two_year_recid$prob,"data/model1_two_year_recid.csv", row.names = TRUE)
 
 # ==========================================================================================
-# model 2
-
-e = empty.graph(nodes)
-modelstring(e) = "[race][sex][age_cat][c_charge_degree|race:sex:age_cat][two_year_recid|race:c_charge_degree]"
+# Model 2
+modelstring(e) = "[race][sex][age_cat][priors_count|race:sex:age_cat][two_year_recid|priors_count:race]"
 dag = model2network(modelstring(e), ordering = nodes)
 graphviz.plot(dag)
 
-model2 = bn.fit(x = dag, data = df)
+model2_race = bn.fit(x = dag, data = df)
 
-write.csv(model2$race$prob,"data/model2_race.csv", row.names = TRUE)
-write.csv(model2$sex$prob,"data/model2_sex.csv", row.names = TRUE)
-write.csv(model2$age_cat$prob,"data/model2_age_cat.csv", row.names = TRUE)
-write.csv(model2$c_charge_degree$prob,"data/model2_c_charge_degree.csv", row.names = TRUE)
-write.csv(model2$two_year_recid$prob,"data/model2_two_year_recid.csv", row.names = TRUE)
+write.csv(model2_race$race$prob,"data/model2_race_race.csv", row.names = TRUE)
+write.csv(model2_race$sex$prob,"data/model2_race_sex.csv", row.names = TRUE)
+write.csv(model2_race$age_cat$prob,"data/model2_race_age_cat.csv", row.names = TRUE)
+write.csv(model2_race$priors_count$prob,"data/model2_race_priors_count.csv", row.names = TRUE)
+write.csv(model2_race$two_year_recid$prob,"data/model2_race_two_year_recid.csv", row.names = TRUE)
 
 # ==========================================================================================
-# model 3
-
-e = empty.graph(nodes)
-modelstring(e) = "[race][sex][age_cat][c_charge_degree|sex:age_cat][two_year_recid|c_charge_degree]"
+# Model 3
+modelstring(e) = "[race][sex][age_cat][priors_count|sex:age_cat][two_year_recid|priors_count]"
 dag = model2network(modelstring(e), ordering = nodes)
 graphviz.plot(dag)
 
-model3 = bn.fit(x = dag, data = df)
+model3_race = bn.fit(x = dag, data = df)
 
-write.csv(model3$race$prob, "data/model3_race.csv", row.names = TRUE)
-write.csv(model3$sex$prob, "data/model3_sex.csv", row.names = TRUE)
-write.csv(model3$age_cat$prob, "data/model3_age_cat.csv", row.names = TRUE)
-write.csv(model3$c_charge_degree$prob, "data/model3_c_charge_degree.csv", row.names = TRUE)
-write.csv(model3$two_year_recid$prob, "data/model3_two_year_recid.csv", row.names = TRUE)
+write.csv(model3_race$race$prob, "data/model3_race_race.csv", row.names = TRUE)
+write.csv(model3_race$sex$prob, "data/model3_race_sex.csv", row.names = TRUE)
+write.csv(model3_race$age_cat$prob, "data/model3_race_age_cat.csv", row.names = TRUE)
+write.csv(model3_race$priors_count$prob, "data/model3_race_priors_count.csv", row.names = TRUE)
+write.csv(model3_race$two_year_recid$prob, "data/model3_race_two_year_recid.csv", row.names = TRUE)
